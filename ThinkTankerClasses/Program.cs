c5d8822c442b975d35ff966842af7556a7224906
Michael Cye Salem <21-1-00193@vsu.edu.ph>
2023-04-12 16:46:33 +0800

    Making a sample access of database in main program.

--- ThinkTankerClasses/Program.cs
+++ ThinkTankerClasses/Program.cs
@@ -1,15 +1,37 @@
-﻿namespace ThinkTankerApp
+﻿using ThinkTankerClasses.Databases;
+
+namespace ThinkTankerApp
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            User[] users = new User[5];
+            //List<User> u = new List<User>();
+            //u.Add(new User("Salem", DateTime.Now, 'M', "fdskfdjf", "12312jdhsj"));
 
+            /*u.Add(new User("kjfsdkjfd", DateTime.Now, 'M', "fdskfdjf", "12312jdhsj"));
+            u.Add(new User("Kaloy", DateTime.Now, 'M', "fdskfdjf", "12312jdhsj"));
+            u.Add(new User("hey", DateTime.Now, 'M', "fdskfdjf", "12312jdhsj"));
+            u.Add(new User("sup", DateTime.Now, 'M', "fdskfdjf", "12312jdhsj"));
 
+            foreach (var user in u)
+            {
+                ProjectDatabase.UserRecord.Add(user);
+            }
+
+            ProjectDatabase.SaveUserRecord();*/
 
             
-            
+
+            ProjectDatabase.UserRecord.Add(new User("Michael Cye R. Salem", DateTime.Now, 'M', "buggernut04", "gwapoko123"));
+
+            //ProjectDatabase.LoadUserRecord();
+
+
+            ProjectDatabase.SaveUserRecord();
+
+
+
         }
     }
 }
