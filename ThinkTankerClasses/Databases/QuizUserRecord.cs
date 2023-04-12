14a1b12f949e532c87b9da23fe650f33175bfb6f
Michael Cye Salem <21-1-00193@vsu.edu.ph>
2023-04-12 16:25:57 +0800

    Added a Class Tests and modified the QuizUserRecord.

--- ThinkTankerClasses/Databases/QuizUserRecord.cs
+++ ThinkTankerClasses/Databases/QuizUserRecord.cs
@@ -12,15 +12,13 @@ namespace ThinkTankerClasses.Databases
         public DateTime LogDateTime { get; set; }
         public User User { get; set; }
         public int Score { get; set; }
-        public QuizType Type { get; set; }
         public int TimeTaken { get; set; }
 
-        public QuizUserRecord(DateTime logDateTime, User user, int score, QuizType quizType, int timeTaken)
+        public QuizUserRecord(DateTime logDateTime, User user, int score, int timeTaken)
         {
             this.LogDateTime = logDateTime;
             this.User = user;
             this.Score = score;
-            this.Type = quizType;
             this.TimeTaken = timeTaken;
         }
     }
