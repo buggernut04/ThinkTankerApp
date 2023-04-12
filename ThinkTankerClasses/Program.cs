d74d5f0382f6d040588ad992e8b4b0a24a45b748
Michael Cye Salem <21-1-00193@vsu.edu.ph>
2023-04-12 15:36:53 +0800

    Revert "Change the test class into one folder."
    This reverts commit 8d8c4ff0cfc2d153604d2e117d124b5d7299594a.

--- ThinkTankerClasses/Program.cs
+++ ThinkTankerClasses/Program.cs
@@ -1,31 +1,13 @@
-﻿using ThinkTankerClasses.Databases;
-
-namespace ThinkTankerApp
+﻿namespace ThinkTankerApp
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            List<User> user = new List<User>();
-
-            user.Add(new User("Cye", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
-            user.Add(new User("Youmu", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
-            user.Add(new User("Kaloy", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
-            user.Add(new User("Alvin", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
-            user.Add(new User("Rea", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
-
-           
-            foreach(var u in user)
-            {
-                ProjectDatabase.UserRecord.Add(u);
-            }
-
-            ProjectDatabase.LoadUserRecord();
-
-            ProjectDatabase.SaveUserRecord();
+            User[] users = new User[5];
 
             
-
+            
         }
     }
 }
