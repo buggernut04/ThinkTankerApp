8d8c4ff0cfc2d153604d2e117d124b5d7299594a
Michael Cye Salem <21-1-00193@vsu.edu.ph>
2023-04-12 14:27:53 +0800

    Change the test class into one folder.

--- ThinkTankerClasses/Program.cs
+++ ThinkTankerClasses/Program.cs
@@ -1,13 +1,31 @@
-﻿namespace ThinkTankerApp
+﻿using ThinkTankerClasses.Databases;
+
+namespace ThinkTankerApp
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            User[] users = new User[5];
+            List<User> user = new List<User>();
+
+            user.Add(new User("Cye", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
+            user.Add(new User("Youmu", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
+            user.Add(new User("Kaloy", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
+            user.Add(new User("Alvin", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
+            user.Add(new User("Rea", DateTime.Now, 'M', "fhjsdhfd", "uriewiur123"));
+
+           
+            foreach(var u in user)
+            {
+                ProjectDatabase.UserRecord.Add(u);
+            }
+
+            ProjectDatabase.LoadUserRecord();
+
+            ProjectDatabase.SaveUserRecord();
 
             
-            
+
         }
     }
 }
